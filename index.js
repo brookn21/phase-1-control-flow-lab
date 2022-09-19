34dbc23872a905f25ba1a8e22132aa31ae8ec47b
Brook Nega <brooknega21@gmail.com>
2022-09-18 22:31:16 -0400

    commit tree 0f6a0030127bf03397fff65d266f2d567587cff5

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if(400 >= num ){
+    return('This one is on me!')
+  }
+  if(400 <= num && num <= 2000){
+    return('That will be twenty bucks.')
+  }
+  if(num > 2000 && num < 2500){
+    return("I will gladly take your thirty bucks.")
+  }
+  if(num > 2500){
+    return("No can do.")
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return(city === "NYC" ? "Ok, sounds good." : "No go.")
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case "generous":
+      return "Thank you so much.";
+    case "not as generous":
+      return "Thank you.";
+    default:
+      return "Bye.";
+  }}
